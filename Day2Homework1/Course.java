9dc4df1f3aa8d1815f15e9916ffa341bad30c9e6
İrem <78559879+iremaliye@users.noreply.github.com>
2021-05-04 16:42:30 +0300

    Add files via upload

---
+++ Day2Homework1/Course.java
@@ -0,0 +1,21 @@
+package odevIký;
+
+public class Course {
+	
+	public Course(int id,String name, String detail,String instructor,String price) {
+		this.id=id;
+		this.name=name;
+		this.detail=detail;
+		this.instructor=instructor;
+		this.price=price;
+		
+	}
+	
+	int id;
+	String name;
+	String detail;
+	String instructor;
+	String price;
+	
+
+}
