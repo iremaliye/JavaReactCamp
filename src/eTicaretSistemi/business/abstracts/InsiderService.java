43e8debe41cea4df8db43cb2549fdc83955d25f7
İrem <78559879+iremaliye@users.noreply.github.com>
2021-05-15 02:37:31 +0300

    Delete src directory

--- src/eTicaretSistemi/business/abstracts/InsiderService.java
+++
@@ -1,9 +0,0 @@
-package eTicaretSistemi.business.abstracts;
-
-public interface InsiderService {
-	void login(String password,String email);
-	void changePassword(String password);
-	void changeEmail(String email);
-	
-
-}
